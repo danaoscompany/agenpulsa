4b091ec561f80af49785a1d77dba8713bd130254
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-04-04 10:07:44 +0700

    a

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -20,15 +20,37 @@ class User extends CI_Controller {
 	}
 	
 	public function update_purchase_status() {
-		/*$refID = $this->input->post('ref_id');
-		$this->db->where('ref_id', $refID);
-		$this->db->update('transactions');*/
-		
 		$data = file_get_contents('php://input');
-		$my_file = 'callback.json';
-		$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
-		fwrite($handle, $data);
-		fclose($handle);
-		echo "Halo dunia";
+		$obj = json_decode($data, true);
+		$status = intval($obj['data']['status']);
+		$refID = $obj['data']['ref_id'];
+		$transaction = $this->db->get_where('transactions', array(
+		    'ref_id' => $refID
+		  ))->row_array();
+		$userID = intval($transaction['user_id']);
+		$fcmToken = $this->db->get_where('users', array(
+		    'id' => $userID
+		  ))->row_array()['fcm_token'];
+		$server_key = 'AAAAon_cfQo:APA91bEL0BldzQJ3z8yJl4ePybkpyvARvRXsyw4tSMJj9ffDWjkzlzBWTZsJnbx3c9hKaaagjC8gIHsFfPeDMK29L70yIwAJtlMuMHHKphUNLc4yHWUoaCZmnuPTG8hAZfZPp1VKN-PX';
+    $url = "https://fcm.googleapis.com/fcm/send";
+    $token = $fcmToken;
+    $serverKey = $server_key;
+    $title = "Informasi Pembayaran";
+    $body = "Ketuk untuk info lebih lanjut";
+    $notification = array('title' =>$title , 'body' => $body, 'sound' => 'default', 'badge' => '1');
+    $arrayToSend = array('to' => $token, 'notification' => $notification,'priority'=>'high');
+    $json = json_encode($arrayToSend);
+    $headers = array();
+    $headers[] = 'Content-Type: application/json';
+    $headers[] = 'Authorization: key='. $serverKey;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST,"POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+    curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+    //Send the request
+    $response = curl_exec($ch);
+    //Close request
+    curl_close($ch);
 	}
 }
