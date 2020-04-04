fb81f46b280413dc99a0676cb55092b58e250c5b
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-04-04 11:51:36 +0700

    a

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -49,7 +49,11 @@ class User extends CI_Controller {
     $title = "Informasi Pembayaran";
     $body = "Ketuk untuk info lebih lanjut";
     $notification = array('title' =>$title , 'body' => $body, 'sound' => 'default', 'badge' => '1');
-    $arrayToSend = array('to' => $token, 'notification' => $notification,'priority'=>'high');
+    $arrayToSend = array('to' => $token, 'notification' => $notification,'priority'=>'high', 'data' => [
+    	'user_id' = "" . $userID,
+    	'ref_id' = $refID,
+    	'payment_status' = "" . $status
+    ]);
     $json = json_encode($arrayToSend);
     $headers = array();
     $headers[] = 'Content-Type: application/json';
